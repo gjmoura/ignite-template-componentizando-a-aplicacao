b114fa7ad85ed2fe064e469cd81e02b6d6edc5cc
Gustavo Jeronimo <gustavo.jeronimo@hotmail.com>
2021-08-26 23:42:35 -0300

    Adiciona as componentes e modulariza o content

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header(title: any) {
+  return(
+    <header>
+      <span className="category">Categoria:<span> {title}</span></span>
+    </header>
+  )
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onClick: any;
+}
+
+export function SideBar(props: SideBarProps, selectedGenreId: number, handleClickButton: any){ 
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
